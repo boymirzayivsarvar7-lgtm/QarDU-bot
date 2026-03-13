bb3484b84189d5ae1c4e27eedab3c32c29855855
Sarvar <boymirzayivsarvar7@gmail.com>
2026-03-13 20:04:32 +0500

    students fix

--- students.py
+++ students.py
@@ -1,5 +1,7 @@
 students = [
 {"id":1,"name":"Ali Valiyev","course":1,"faculty":"IT","contract_debt":0},
+
 {"id":2,"name":"Bekzod Karimov","course":2,"faculty":"IT","contract_debt":1200000},
+
 {"id":3,"name":"Jasur Xasanov","course":3,"faculty":"Economics","contract_debt":0}
 ]
